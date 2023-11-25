7ae967c817bde32d1ef7c1a7c7656b7999f35ca6
dbarfa <contact@dbarfa.com>
2023-11-25 15:17:29 +0100

    initial breaking modification

--- flake.nix
+++ flake.nix
@@ -1,16 +1,16 @@
 {
   description = "dbarfa's NixOS Flake";
   
-
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+	url = "github:nix-community/home-manager";
+	inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   nixConfig = {
     experimental-features = [ "nix-command" "flakes" ];
-
     extra-substituters = [
       "https://nix-community.cachix.org"
     ];
@@ -19,34 +19,24 @@
     ];
   };
 
-  # A flake can have many use cases and different types of outputs.
-  # 
-  # parameters in function `outputs` are defined in `inputs` and
-  # can be referenced by their names. However, `self` is an exception,
-  # this special parameter points to the `outputs` itself(self-reference)
-  # 
-  # The `@` syntax here is used to alias the attribute set of the
-  # inputs's parameter, making it convenient to use inside the function.
-  outputs = { 
-	self, 
-	nixpkgs,
-	home-manager,
-	... }@inputs: {
-    nixosConfigurations = {
-      dbarfa = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-	specialArgs = inputs;
-        modules = [
-          ./configuration.nix
-	  home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
+  outputs = imputs@{ self, nixpkgs,home-manager,... }: 
+    let 
+    	vars = { user = "dbarfa"; };
+    in {
+    	nixosConfigurations = {
+      	    dbarfa = nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+	    specialArgs = inputs;
+            modules = [
+              ./configuration.nix
+	      home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
 
-            home-manager.users.dbarfa = import ./home.nix;
-          }
-        ];
+                home-manager.users.dbarfa = import ./home.nix;
+              }
+            ];
+          };
+        };
       };
-    };
-  };
-}
