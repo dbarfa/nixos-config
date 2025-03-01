c7b5d80f66f1a857a2ecf8998ca8702afb6a8040
Daniel-George Barfa <contact@dbarfa.com>
2025-03-01 20:02:11 +0100

    add new git default configs

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -7,6 +7,20 @@
       safe = {
         directory = "*";
       };
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      column.ui = "auto";
+      branch.sort = "-committerdate";
+      tag.sort = "version:refname";
+      push = {
+        default = "simple";
+        autoSetupRemote = false;
+      };
+      commit.verbose = true;
     };
   };
 }
