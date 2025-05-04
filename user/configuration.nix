69f1e854d8cf4a4268050c8defc3949742002ea6
Daniel-George Barfa <contact@dbarfa.com>
2025-05-04 22:34:15 +0200

    format configuration.nix

--- user/configuration.nix
+++ user/configuration.nix
@@ -4,16 +4,23 @@
   imports = [ ./desktop/hardware-configuration.nix ];
 
   nixpkgs.config = { allowUnfree = true; };
-
   nix = { settings = { experimental-features = [ "nix-command" "flakes" ]; }; };
+
   hardware = {
     bluetooth = {
       enable = true;
       powerOnBoot = true;
     };
+    pulseaudio.enable = false;
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = false;
+      powerManagement.finegrained = false;
+      open = false;
+      nvidiaSettings = true;
+      package = config.boot.kernelPackages.nvidiaPackages.stable;
+    };
   };
-
-  environment.localBinInPath = true;
 
   programs.fish.enable = true;
   users.defaultUserShell = pkgs.fish;
@@ -30,25 +37,24 @@
     wlr = { enable = true; };
   };
 
-  environment.systemPackages = with pkgs; [ git wget neovim curl dig ];
-
-  environment.variables.EDITOR = "nvim";
-  
-  networking = {
-    hostName = "dbarfa";
-    wireguard = {
-      enable = true;
-    };
-    networkmanager = {
-      enable = true;
-    }; 
-    firewall = {
-      checkReversePath = "loose";  
+  environment = {
+    systemPackages = with pkgs; [ git wget neovim curl dig ];
+    localBinInPath = true;
+    variables = {
+      EDITOR = "nvim";
     };
   };
 
-  time.timeZone = "Europe/Brussels";
-  time.hardwareClockInLocalTime = true;
+  networking = {
+    hostName = "dbarfa";
+    wireguard.enable = true;
+    networkmanager.enable = true;
+    firewall.checkReversePath = "loose";  
+  };
+  time = {
+    timeZone = "Europe/Brussels";
+    hardwareClockInLocalTime = true;
+  };
 
   i18n.defaultLocale = "en_US.UTF-8";
 
@@ -68,13 +74,13 @@
       xterm.enable = false;
       wallpaper.mode = "fill";
     };
+    videoDrivers = [ "nvidia" ];
     xkbVariant = "";
     xkbOptions = "ctrl:nocaps";
   };
 
   # services.printing.enable = true;
 
-  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -83,20 +89,7 @@
     pulse.enable = true;
   };
 
-
   virtualisation.vmware.guest.enable = true;
-
-  services.xserver.videoDrivers = [ "nvidia" ];
-  hardware.nvidia = {
-    modesetting.enable = true;
-  
-      powerManagement.enable = false;
-      powerManagement.finegrained = false;
-  
-      open = false;
-      nvidiaSettings = true;
-      package = config.boot.kernelPackages.nvidiaPackages.stable;
-    };
 
   boot = {
     loader = { systemd-boot.enable = true; };
