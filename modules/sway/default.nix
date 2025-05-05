18078ffbc873873d8bb2f6fe37d4e4f1b3296415
Daniel-George Barfa <contact@dbarfa.com>
2025-05-05 21:57:28 +0200

    sway: expand configuration and provide var for dynamic monitor

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -1,11 +1,13 @@
-{ config, pkgs, lib, ... }:
+{ pkgs, lib, ... }:
 let
   leftMonitor = "Microstep MSI G2712 0x000013D2";
   rightMonitor = "Microstep MSI G2712 0x0000089A";
+  activeMonitor = "$(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name')";
 in
 {
   wayland.windowManager.sway = {
     enable = true;
+
     config = rec {
       modifier = "Mod1";
       terminal = "ghostty";
@@ -26,11 +28,33 @@ in
           xkb_options = "ctrl:nocaps";
         };
       };
+      
+      focus = {
+        followMouse = false;
+      };
+
+      gaps.inner = 5;
+
+      workspaceOutputAssign = [
+        { output = leftMonitor;  workspace = "1"; }
+        { output = rightMonitor; workspace = "2"; }
+      ];
+
+      window = {
+        border = 2;
+        titlebar = false;
+      };
+
+      keybindings = lib.mkOptionDefault {
+        # rofi: dmenu
+        #"${modifier}+d" = "exec ${pkgs.rofi}/bin/rofi -show drun -monitor ${activeMonitor}";
+        # rofi: clipboard
+      };
     };
+
+    # maybe - color, keybindings, volume knob kb, remove useless bar info
+
     extraConfig = ''
-      workspace "1" output "${leftMonitor}"
-      workspace "2" output "${rightMonitor}"
     '';
   };
 }
-
